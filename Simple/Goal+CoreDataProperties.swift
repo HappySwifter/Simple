63b6c350f7ac50ce3109a97d4b43aef3e0701e4e
Артем Валиев <a_valiev@controlsystems.ru>
2016-06-29 12:01:47 +0300

    commit

---
+++ Simple/Goal+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Goal+CoreDataProperties.swift
+//  Simple
+//
+//  Created by Артем Валиев on 29.06.16.
+//  Copyright © 2016 Артем Валиев. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Goal {
+
+    @NSManaged var timeStamp: NSDate?
+    @NSManaged var name: UNKNOWN_TYPE
+
+}
---
+++ Simple/Goal.swift
@@ -0,0 +1,17 @@
+//
+//  Goal.swift
+//  Simple
+//
+//  Created by Артем Валиев on 29.06.16.
+//  Copyright © 2016 Артем Валиев. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Goal: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
